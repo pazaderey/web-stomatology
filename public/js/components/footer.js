28a5e6dc3267cf353f4dd8e5d5d20acda2f5ba0a
Petr <pazaderey@edu.hse.ru>
2023-10-06 23:22:04 +0300

    ✨ Upgrade footer

--- public/js/components/footer.js
+++ public/js/components/footer.js
@@ -2,21 +2,91 @@ const footerTemplate = document.createElement("template");
 footerTemplate.innerHTML = `
 <style>
 #footer {
-    height: 3rem;
+    height: 11rem;
     background-color: #BD81C9;
     display: flex;
     flex-direction: row;
     align-items: center;
 }
-.copyright {
+
+#footer-container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+
+.footer-item {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.footer-contacts {
+    background-color: #353334;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+
+.footer-copyright {
+    height: 3rem;
+}
+
+span.contacts {
+    text-align: center;
+}
+
+span.name {
+    font-size: 1.5rem;
+    color: #ffffff;
+}
+
+span.email {
+    font-size: 1.2rem;
+    color: #ffffff;
+}
+
+div.contact-items {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    gap: 3%;
+}
+
+img.contact-icon {
+    height: 30px;
+    width: 30px;
+}
+
+span.copyright {
     text-align: center;
     font-size: 1.2rem;
-    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-    padding-left: 5rem;
-
+    padding-left: 5%;
+    color: #000000;
 }
+
 </style>
-<footer id="footer"><span class="copyright">Copyright © HSE MIEM</span></footer>
+<footer id="footer">
+    <div id="footer-container">
+        <div class="footer-item footer-contacts">
+            <span class="name">
+                Задерей Петр Алексеевич
+            </span>
+            <span class="email">
+                pazaderey@edu.hse.ru
+            </span>
+            <div class="contact-items">
+                <a href="https://t.me/pazaderey" target="_blank"><img class="contact-icon" src="../img/telegram.png"/></a>
+                <a href="https://github.com/pazaderey" target="_blank"><img class="contact-icon" src="../img/social.png"/></a>
+            </div>
+        </div>
+        <div class="footer-item footer-copyright"><span class="copyright">Copyright © HSE MIEM</span></div>
+    </div>
+</footer>
 `;
 
 class Footer extends HTMLElement {
--- public/js/components/header.js
+++ public/js/components/header.js
@@ -2,6 +2,7 @@ const headerTemplate = document.createElement("template");
 headerTemplate.innerHTML = `
 <style>
 #header {
+    width: 100%;
     background-color: #BD81C9;
     height: 4.5rem;
     display: flex;
@@ -14,9 +15,15 @@ headerTemplate.innerHTML = `
     width: 4rem;
 }
 
+#menu-img {
+    height: 2rem;
+    width: 2rem;
+}
+
 #nav {
     width: 100%;
-    padding-left: 5rem;
+    padding-left: 5%;
+    padding-right: 5%;
     align-self: center;
     display: flex;
     flex-direction: row;
@@ -25,26 +32,87 @@ headerTemplate.innerHTML = `
     gap: 1%;
 }
 
-.nav-link {
-    text-decoration: none;
+a.nav-link {
+    text-decoration-color: #000000;
+    text-decoration-line: none;
+    color: #000000;
 }
 
 .nav-elem {
     height: 2.5rem;
-    padding-left: 1rem;
-    padding-right: 1rem;
+    padding-left: 1%;
+    padding-right: 1%;
     text-align: center;
     font-size: 1.5rem;
-    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
     display: flex;
     justify-content: center;
     align-items: center;
+}
+
+.dropdown {
+    display: none;
+}
+
+.dropdown-options {
+    display: none;
+    overflow: auto;
+    position: absolute;
+    left: -8rem;
+    background-color:#fff;
+    border-radius: 5px;
+    box-shadow: 0px 10px 10px 0px rgba(0,0,0,0.4);
+}
+
+.dropdown-options a {
+    display: block;
+    color: #000000;
+    text-decoration: none;
+    padding: 15px 30px;
+  }
+
+@media (max-width: 550px) {
+    .nav-elem {
+        display: none;
+    }
+
+    #nav {
+        justify-content: space-between;
+    }
+
+    .dropdown {
+        display: inline-block;
+        position: relative;
+    }
+
+    .dropdown:hover .dropdown-options {
+        display: block;
+    }
+
+    .nav-btn {
+        height: 2.5rem;
+        width: 2.5rem;
+        display: block;
+        border: none;
+        background-color: inherit;
+        cursor: pointer;
+    }
 }
 </style>
 <header id="header">
     <nav id="nav">
         <a class="nav-link" href="/"><img src="../img/logo.svg" id="logo" /></a>
-        <div class="nav-elem"><a class="nav-link" href="/detect"><span>Обнаружение</span></a></div>
+
+        <div class="dropdown">
+            <button class="nav-btn"><img src="../img/menu.png" id="menu-img"/></button>
+            <div class="dropdown-options">
+                <a href="/detect">Диагностика</a>
+                <a href="/help">Помощь</a>
+                <a href="/feedback">Отзывы</a>
+                <a href="/about">О нас</a>
+            </div>
+        </div>
+
+        <div class="nav-elem"><a class="nav-link" href="/detect"><span>Диагностика</span></a></div>
         <div class="nav-elem"><a class="nav-link" href="/help"><span>Помощь</span></a></div>
         <div class="nav-elem"><a class="nav-link" href="/feedback"><span>Отзывы</span></a></div>
         <div class="nav-elem"><a class="nav-link" href="/about"><span>О нас</span></a></div>
